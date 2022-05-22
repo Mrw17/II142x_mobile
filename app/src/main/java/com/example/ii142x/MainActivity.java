396155bbaac7e6ba2d2f621b498102b8d6eb0018
Daniel <Danielwesterlund17@gmail.com>
2022-05-22 16:16:18 +0200

    Added listeners for buttons in MainActivity

--- app/src/main/java/com/example/ii142x/MainActivity.java
+++ app/src/main/java/com/example/ii142x/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.ii142x;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 import com.example.ii142x.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
+    Button btnHeartBeat;
+    Button btnGPS;
+    Button btnPressure;
+    Button btnAccelerometer;
+    Button btnPhoto;
     private ActivityMainBinding binding;
 
     @Override
@@ -17,6 +22,41 @@ public class MainActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mTextView = binding.text;
+        setUpGUI();
     }
+
+    private void setUpGUI(){
+        btnHeartBeat = findViewById(R.id.btnGetHeartBeat);
+        btnHeartBeat.setOnClickListener(v-> sendUserToHeartBeatActivity());
+
+        btnGPS = findViewById(R.id.btnGetGPS);
+        btnGPS.setOnClickListener(v-> sendUserToGPSActivity());
+
+        btnPressure = findViewById(R.id.btnGetPressure);
+        btnPressure.setOnClickListener(v-> sendUserToPressureActivity());
+
+        btnAccelerometer = findViewById(R.id.btnAccelerometer);
+        btnAccelerometer.setOnClickListener(v-> sendUserToAccelerometerActivity());
+
+        btnPhoto = findViewById(R.id.btnGetPhoto);
+        btnPhoto.setOnClickListener(v-> sendUserToPhotoActivity());
+        
+    }
+
+    private void sendUserToHeartBeatActivity() {
+    }
+
+    private void sendUserToGPSActivity() {
+    }
+
+    private void sendUserToPressureActivity() {
+    }
+
+    private void sendUserToAccelerometerActivity() {
+    }
+
+    private void sendUserToPhotoActivity() {
+    }
+
+
 }
